6ff4cba11effd50ef4bd9b29446cf29025a317fe
anjie_f <amfarmer2022@gmail.com>
2025-10-21 18:32:49 -0500

    Feat:Add success message to js, html,css

--- js/script.js
+++ js/script.js
@@ -1,22 +1,30 @@
 let emailInput = document.querySelector("#email-input");
 let emailForm = document.querySelector(".email-form");
 let errorMessage = document.querySelector(".error-message");
+let successMessage = document.querySelector(".success-message");
+let requestGranted = document.querySelector("#request-granted");
 
 emailForm.addEventListener("submit", function (event) {
 
     event.preventDefault();
 
     if (emailInput.checkValidity() === false) {
-        emailInput.placeholder = "jane#email.com";
         emailForm.classList.add("has-error");
         errorMessage.classList.remove("visually-hidden");
+        successMessage.classList.add("visually-hidden");
     } else {
         emailForm.classList.remove("has-error");
         errorMessage.classList.add("visually-hidden");
+        successMessage.classList.remove("visually-hidden");
         emailInput.value = "";
-        emailInput.placeholder = "Email address"; 
+        requestGranted.innerText = "On the list";
     }
 });
+
+setTimeout(function () {
+    requestGranted.style.display = 'none';
+}, 3000);
+
 
 emailInput.addEventListener("mouseover", function () {
     emailInput.placeholder = "janedoe@gmail.com";
